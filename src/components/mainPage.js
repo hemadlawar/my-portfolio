a81bbb322cd7d54c356961b8f250e48d312d2347
hemaddlawar <hema.200219627512@gmail.com>
2024-01-21 00:54:44 +0300

    modify navigation bar

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavList from "./navList";
+import MainSection from "./mainSection";
 export function MainPage() {
   return (
     <div>
       <NavList />
-      
+      <MainSection />
     </div>
   );
 }
--- src/components/navList.js
+++ src/components/navList.js
@@ -5,122 +5,73 @@ export default function NavList() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <>
-      <nav className="bg-gray-400">
-        <div className="flex items-center justify-between h-16 px-4 border-b border-solid border-slate-600">
-          <div className="flex-shrink-0 font-bold tracking-wider">LOGO</div>
-          <button
-            type="button"
-            className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
-            onClick={() => setShowMobileMenu(!showMobileMenu)}
+      <nav className=" border-gray-200 dark:bg-gray-900 flex flex-wrap items-center">
+        <div className="ml-2 mt-0 ">
+          <p className="block py-3 px-4 text-black bg-yellow-500 rounded-md md:bg-transparent md:text-blue-500 md:p-0 font-bold text-lg dark:text-white md:dark:text-blue-500  ">
+            HEMA DLAWAR
+          </p>
+        </div>
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-default "
           >
-            <svg
-              className="h-6 w-6"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  skills
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Projects
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Educations
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-      {showMobileMenu && (
-        <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
-          <a
-            href="#"
-            className="block md:inline-block px-3 py-2 rounded-md text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-          >
-            Home
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >
-            educations
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >
-            skills
-          </a>
-          <a
-            href="#"
-            className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >
-            Contact
-          </a>
-        </div>
-      )}
     </>
   );
 }
-/**
- * 
- * 
- * 
- * <nav className="bg-gray-400">
-      <div className="flex items-center justify-between h-16 px-4 border-b border-solid border-slate-600">
-        <div className="flex-shrink-0 font-bold tracking-wider">
-          LOGO
-        </div>
-        <button
-          type="button"
-          className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
-          onClick={() => setShowMobileMenu(!showMobileMenu)}>
-          <svg
-            className="h-6 w-6"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      {showMobileMenu && <Menu />}
-    </nav>
-    -------
-
-
-
-
-     <div>
-      <div className="px-2 py-3 space-y-2 font-medium text-slate-700">
-        <a
-          href="#"
-          className="block px-3 py-2 rounded-md text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-        >
-          Home
-        </a>
-        <a
-          href="#"
-          className="block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-        >
-          Features
-        </a>
-        <a
-          href="#"
-          className="block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-        >
-          Pricing
-        </a>
-        <a
-          href="#"
-          className="block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-        >
-          Contact
-        </a>
-      </div>
-    </div>
- */
