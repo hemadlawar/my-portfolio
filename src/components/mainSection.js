139cf625264fe201ec4e690cb83b6e2e2fb1d7ad
hemaddlawar <hema.200219627512@gmail.com>
2024-01-24 00:29:36 +0300

    change the width of main section

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function MainSection() {
   return (
-    <div className="relative flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ml-8 mt-10">
+    <div className="relative flex  flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ml-8 mt-10  w-11/12 bg-slate-600 ">
       <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border">
         <img
           src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
           alt="ui/ux review check"
         />
       </div>
+
       <div className="p-6">
         <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
           UI/UX Review Check
         </h4>
         <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
-          Because it&apos;s about motivating the doers. Because I&apos;m here to
-          follow my dreams and inspire others.
+          hello my name is hema dlawar i'm 22 years old ... regards
         </p>
       </div>
       <div className="flex items-center justify-between p-6 ">
