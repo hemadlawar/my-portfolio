94bf60d11541dc9420c43c10a201b575a2ea867e
hemaddlawar <hema.200219627512@gmail.com>
2024-02-01 20:54:47 +0300

    add a button to main page

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -19,7 +19,9 @@ export default function MainSection() {
             <br /> Learning is my thing, and I'm always eager to tackle new
             topics
           </p>
-          <button>my resume</button>
+          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-12 w-56 h-12">
+            View my Resume
+          </button>
         </div>
 
         {/** portfolio picture */}
