7884ed7ecb3cb4e9d40fef60f7eb2a03c4caec03
hemaddlawar <hema.200219627512@gmail.com>
2024-02-12 23:23:46 +0300

    add the skilss

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function skills() {
-  return <div>skills</div>;
+  return (
+    <div>
+      this is my skils:
+      <div>html</div>
+      <div> java</div>
+      <div>java script</div>
+      <div>node js</div>
+    </div>
+  );
 }
